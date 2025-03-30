2d34710c6eca60a66d8ebefab9edc69a3edc9266
Yannick Debrée <yannickdebree@ask-ell.com>
2025-03-30 15:34:39 +0200

    README file created

--- docker-build-and-push-to-public-registry.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-curl -O http://gitlab.in.ask-ell.com/shared/scripts/-/raw/release/docker-build-and-push.sh
-chmod +x ./docker-build-and-push.sh
-export DOCKER_REGISTRY_TOKEN=$ASKELL_CI_PUBLIC_DOCKER_REGISTRY_TOKEN
-export DOCKER_REGISTRY_DOMAIN=$ASKELL_CI_PUBLIC_DOCKER_REGISTRY_DOMAIN
-./docker-build-and-push.sh gitlab-k8s-deployer
